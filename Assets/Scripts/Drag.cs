3a7a62d7b9c84938b75db67b31cf6c8a8fa1b7cb
qwertyh <sg3555@naver.com>
2020-03-09 18:27:08 +0900

    점프간판(controller)구현

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -9,16 +9,21 @@ public class Drag : MonoBehaviour
     bool startgame; //게임시작 bool
     Vector2 originposition, pastposition;   //각각 최초위치, 마우스를 놓기전 위치
     bool deadlock;  //타일이 다른 객체에 겹치는것을 방지하기 위한 교착상태 bool
+    SpriteRenderer thissprite;
     SpriteRenderer[] tiles; //자식객체의 타일들
     Rigidbody2D rigid;
+    Collider2D colid;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         tiles = gameObject.GetComponentsInChildren<SpriteRenderer>();
+        colid = GetComponent<Collider2D>();
+        thissprite = GetComponent<SpriteRenderer>();
         originposition = this.gameObject.transform.position;
         deadlock = false;
         startgame = false;
+        
     }
 
     //타일이 다른 객체에 겹쳤을 경우
@@ -77,9 +82,13 @@ public class Drag : MonoBehaviour
     private void OnMouseDown()
     {
         if(!startgame)
+        {
             //그 순간의 타일 좌표 저장
             //다른 타일과 겹친 상태(교착상태)에 마우스를 놓을 경우 이 좌표로 이동
             pastposition = this.gameObject.transform.position;
+            rigid.bodyType = RigidbodyType2D.Dynamic;
+        }
+            
     }
 
     //마우스를 드래그해서 타일을 옮기는 함수
@@ -97,17 +106,29 @@ public class Drag : MonoBehaviour
     //마우스를 드래그한 상태에서 놓은 순간
     private void OnMouseUp()
     {
+        //다른 타일과 겹친 상태(교착상태)에 마우스를 놓을 경우 이 좌표로 이동
         if (deadlock)
-            //다른 타일과 겹친 상태(교착상태)에 마우스를 놓을 경우 이 좌표로 이동
             transform.position = pastposition;
+        rigid.bodyType = RigidbodyType2D.Static;
+
     }
 
     //Start버튼을 눌렀을 경우
     public void StartMove()
     {
         //rigidbody타입 변경(Dynamic상태를 방치하면 캐릭터가 밟을 때 미세하게 움직임
+        colid.isTrigger = false;
         rigid.bodyType = RigidbodyType2D.Static;
         startgame = true;
+
+        //안내판일 경우 colide속성을 trigger로 변경, 반투명화
+        if (this.tag.Contains("Jumper"))
+        {
+            colid.isTrigger = true;
+            thissprite.color  = new Color(255, 255, 255, 0.5f);
+        }
+            
+            
     }
 
     //Stop버튼을 눌렀을 경우
@@ -116,6 +137,11 @@ public class Drag : MonoBehaviour
         //다시 타일을 움직일 수 있는 상태로 변경
         rigid.bodyType = RigidbodyType2D.Dynamic;
         startgame = false;
+        if (this.tag.Contains("Jumper"))
+        {
+            colid.isTrigger = false;
+            thissprite.color = new Color(255, 255, 255, 1);
+        }
     }
 
     //Reset버튼을 눌렀을 경우
@@ -127,6 +153,12 @@ public class Drag : MonoBehaviour
         //타일들의 위치를 게임 시작 직후의 위치로 변경
         //후에 UI로 변경하여 타일들을 UI로 옮기는 작업을 실시해야함
         transform.position = originposition;
+
+        if (this.tag.Contains("Jumper"))
+        {
+            colid.isTrigger = false;
+            thissprite.color = new Color(255, 255, 255, 1);
+        }
     }
 
     
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -6,16 +6,18 @@ using UnityEngine;
 //버튼을 누르면 캐릭터가 움직이는 Actor객체의 스크립트
 public class Move : MonoBehaviour
 {
+    Vector2 originPosition;
     Rigidbody2D rigid;
     Animator anim;  //스프라이트 애니메이션 담당
     public bool startGame; //게임 시작상태 bool
-    Vector2 originPosition;
-
+    public float height;
+    
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         startGame = false;
+        height = 18;
 
         //게임시작전에 Actor객체를 정적상태로 변경(움직임 방지)
         rigid.bodyType = RigidbodyType2D.Static;
@@ -26,8 +28,18 @@ public class Move : MonoBehaviour
     void FixedUpdate()
     {
         //게임 시작상태에서만 움직임 활성화
-        if(startGame == true)
-            rigid.velocity = new Vector2(3, rigid.velocity.y);
+        if (startGame == true)
+            rigid.velocity = new Vector2(5, rigid.velocity.y);
+
+        //점프후 바닥에 착지시 점프애니메이션 종료
+        if(rigid.velocity.y<0)
+        {
+            RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, LayerMask.GetMask("platform"));
+            if (rayHit.collider != null)
+                if(rayHit.distance < 0.5f)
+                    anim.SetBool("IsJump", false);
+        }
+        
     }
 
     //게임 시작
@@ -45,6 +57,16 @@ public class Move : MonoBehaviour
         gameObject.transform.position = originPosition;
         startGame = false;
         anim.SetBool("IsMove", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //점프
+        if(collision.tag.Contains("Jumper") && !anim.GetBool("IsJump") && startGame)
+        {
+            rigid.AddForce(Vector2.up * height, ForceMode2D.Impulse);
+            anim.SetBool("IsJump", true);
+        }
     }
     
 }

3dd9b98bd40518048ab61dc7620bac5fd7354f9f
minseong <decidone@gmail.com>
2020-07-24 17:23:35 +0900

    게임 시작시 달리는 스크립트 추가

---
+++ Assets/MetalSlug/Scripts/MS_GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MS_GameManager : MonoBehaviour
+{
+    public MS_PlayerController player; //플레이어블 캐릭터
+    public Button[] buttons = new Button[3]; //시작, 정지, 초기화 버튼
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //캐릭터 행동개시
+    public void startUpGame()
+    {
+        player.StartMove();
+    }
+
+    //버튼 비활성화 함수
+    public void disableButton()
+    {
+        buttons[0].interactable = false;
+        buttons[1].interactable = false;
+        buttons[2].interactable = false;
+    }
+
+    //버튼 활성화 함수
+    public void enableButton()
+    {
+        buttons[0].interactable = true;
+        buttons[1].interactable = true;
+        buttons[2].interactable = true;
+    }
+}
---
+++ Assets/MetalSlug/Scripts/MS_PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MS_PlayerController : MonoBehaviour
+{
+    Vector2 originPosition; //캐릭터 최초위치
+    Rigidbody2D rigid; //물리엔진
+    Animator anim;  //스프라이트 애니메이션 담당
+    AudioSource audioSource; //소리제어자
+    Collider2D col; //충돌제어자
+    SpriteRenderer sprite; //스프라이트제어자
+
+    public bool startGame;  //게임 시작상태 bool
+    public bool clear; //클리어 여부 
+    public float maxSpeed = 5f;    // Player Speed
+    public float jumpForce = 600;   // Player jump force
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        col = GetComponent<Collider2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGame = false;
+        clear = false;
+    }
+
+    void FixedUpdate()
+    {
+        //게임 시작상태에서만 움직임 활성화
+        if (startGame)
+        {
+            rigid.AddForce(Vector2.right * 1f, ForceMode2D.Impulse);
+            
+            //최대속력 설정
+            if (rigid.velocity.x > maxSpeed && !clear)
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            else if (rigid.velocity.x < -maxSpeed && !clear)
+                rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+
+            if (Mathf.Abs(rigid.velocity.x) > 0.3)
+            {
+                anim.SetBool("isRunning", true);
+            }
+            else
+                anim.SetBool("isRunning", false);
+        }
+    }
+
+    public void StartMove()
+    {
+        Debug.Log("start");
+        startGame = true;
+    }
+}

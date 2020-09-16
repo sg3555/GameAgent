499a7ebbb5d4b85e3ac5a1ad7c52ba6cdc2fbbf2
minseong <decidone@gmail.com>
2020-09-16 18:30:51 +0900

    main bgm 추가

--- Assets/MetalSlug/Scripts/MS_GameManager.cs
+++ Assets/MetalSlug/Scripts/MS_GameManager.cs
@@ -8,7 +8,7 @@ public class MS_GameManager : MonoBehaviour
 {
     public MS_PlayerController player; //플레이어블 캐릭터
     public Button[] buttons = new Button[3]; //시작, 정지, 초기화 버튼
-
+    public BgmController MainBGM, DeadBGM, Goal1, Goal2; //배경음 관리자
     public Drager[] MovableTile, Inventory; //아이템창
     public CamControl mainCam; //카메라
     public GameObject ClearUI, ExplainUI; //깃발, 클리어UI, 설명창UI
@@ -17,6 +17,10 @@ public class MS_GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        MainBGM.PlaySound();
+        MainBGM.SetLoop(true);
+        MainBGM.SetVolume(0.7f);
+
         isopen = false;
         ExplainUI.SetActive(isopen);
 
@@ -29,6 +33,7 @@ public class MS_GameManager : MonoBehaviour
         //클리어 여부 체크
         if (player.getState().Equals("clear"))
         {
+            MainBGM.StopSound();
             clearScreen();
             //Debug.Log(player.getState());
         }
@@ -43,7 +48,7 @@ public class MS_GameManager : MonoBehaviour
             dr.StartGame();
         foreach (Drager dr in Inventory)
             dr.StartGame();
-        //MainBGM.SetVolume(1.0f);
+        MainBGM.SetVolume(1.0f);
     }
 
     public void stopButton()
@@ -54,7 +59,7 @@ public class MS_GameManager : MonoBehaviour
             dr.StopGame();
         foreach (Drager dr in Inventory)
             dr.StopGame();
-        //MainBGM.SetVolume(0.7f);
+        MainBGM.SetVolume(0.7f);
     }
 
     public void resetButton()
@@ -66,7 +71,7 @@ public class MS_GameManager : MonoBehaviour
             dr.ResetGame();
         foreach (Drager dr in Inventory)
             dr.ResetGame();
-        //MainBGM.SetVolume(0.7f);
+        MainBGM.SetVolume(0.7f);
     }
 
     //버튼 비활성화 함수

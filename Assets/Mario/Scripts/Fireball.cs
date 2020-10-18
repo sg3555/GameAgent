ca8ad20ba4fd3a8e0c2af96ec3767798b8f1362c
qwertyh <sg3555@naver.com>
2020-10-18 21:46:22 +0900

    no message

---
+++ Assets/Mario/Scripts/Fireball.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public Rigidbody2D rigid;
+    public Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        velocity = rigid.velocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigid.velocity.y < velocity.y)
+        {
+            rigid.velocity = velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rigid.velocity = new Vector2(velocity.x, -velocity.y);
+
+        if(collision.contacts[0].normal.x != 0)
+        {
+
+        }
+    }
+
+    void Explode()
+    {
+        Destroy(this.gameObject);
+    }
+}

9a6fc988d0e6b48446911dd186e087aecef64bb9
jinsupark <gary3545@naver.com>
2020-11-14 17:06:54 +0900

    점프 수정사항

--- Assets/rockman/scripts/rm_move.cs
+++ Assets/rockman/scripts/rm_move.cs
@@ -29,6 +29,8 @@ public class rm_move : MonoBehaviour
     public rm_enemy[] enemy;
     public bool isladder = false;
     public float distance;
+    private int SignLayerNum = 13; // ground의 레이어 번호
+    private int playerLayerNum = 19;
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -187,7 +189,7 @@ public class rm_move : MonoBehaviour
         {
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 7,LayerMask.GetMask("ground"));
-            
+            Physics2D.IgnoreLayerCollision(playerLayerNum, SignLayerNum, false);
             if (rayHit.collider != null)
             {
                     if (rayHit.distance <= 1f)
